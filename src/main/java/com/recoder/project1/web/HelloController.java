ef9ea1d5aaac48e732de8e0d65f63ad05d744e68
raiders032 <nameks@naver.com>
2020-07-10 02:37:56 +0900

    Person Entity 직접 노출

--- src/main/java/com/recoder/project1/web/HelloController.java
+++ src/main/java/com/recoder/project1/web/HelloController.java
@@ -1,15 +1,28 @@
 package com.recoder.project1.web;
 
 
+import com.recoder.project1.domain.person.Person;
+import com.recoder.project1.domain.person.PersonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
 
+    private final PersonRepository personRepository;
+
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    @ResponseBody
+    public List<Person> hello() {
+        List<Person> all = personRepository.findAll();
+        return all;
     }
+
+
 }
