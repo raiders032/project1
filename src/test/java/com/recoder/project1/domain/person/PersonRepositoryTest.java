1c50a38c277557d43512b658bbdd127189c8b757
raiders032 <nameks@naver.com>
2020-07-10 13:40:20 +0900

    Revert "domain 설정"
    This reverts commit c6e052f2

--- src/test/java/com/recoder/project1/domain/person/PersonRepositoryTest.java
+++ src/test/java/com/recoder/project1/domain/person/PersonRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -16,7 +15,6 @@ import static com.recoder.project1.domain.person.QPerson.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
 class PersonRepositoryTest {
 
     @Autowired
